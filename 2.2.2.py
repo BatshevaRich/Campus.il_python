7d85d585e0a5fe8e5d92263f6d793d64c2f001b1
Batsheva Rich <batsheva7424@gmail.com>
2020-06-03 20:13:49 +0300

    animal class

---
+++ 2.2.2.py
@@ -0,0 +1,18 @@
+class Animal():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def birthday(self):
+        self.age += 1
+
+    def get_age(self):
+        return self.age
+
+def main():
+    deer = Animal('deer', 2)
+    dolphin = Animal('dolphine', 3)
+    deer.birthday()
+    print('{}, {}'.format(deer.age, dolphin.age))
+
+main()
